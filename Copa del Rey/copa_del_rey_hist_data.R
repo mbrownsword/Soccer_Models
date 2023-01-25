56ab84c24d91a06397acc733dd28663ea0be9dbe
mbrownsword <mbrownsword6@gmail.com>
2023-01-24 20:23:51 -0800

    Add files via upload

---
+++ Copa del Rey/copa_del_rey_hist_data.R
@@ -0,0 +1,103 @@
+library(tidyverse)
+library(textclean)
+library(data.table)
+library(flexdashboard)
+library(rvest)
+library(googlesheets)
+library(readxl)
+library(raster)
+library(tidyverse)
+library(data.table)
+library(tools)
+library(httr)
+library(RSelenium)
+library(jsonlite)
+library(RSelenium)
+library(httr)
+library(rvest)
+library(ttutils)
+library(jsonlite)
+library(remotes)
+library(emayili)
+library(curl)
+library(openssl)
+library(sys)
+library(mailR)
+library(knitr)
+library(png) 
+library(mailR)
+library(DT)
+library(webshot)
+library(stringdist)
+detach(package:plyr)
+detach(package:dplyr)
+library(plyr)
+library(dplyr)
+library(curl)
+library(openssl)
+library(sys)
+library(mailR)
+library(printr)
+library(png)
+library(mailR)
+library(DT)
+library(webshot)
+library(ggpubr)
+library(png)
+library(grid)
+library(gridExtra)
+library(tidyquant)
+library(pracma)
+library(magrittr)
+library(haven)
+library(stringi)
+library(worldfootballR)
+getwd()
+############ League Analysis ################
+
+setwd("C:/Users/CORSAIR GAMING/Documents/FotMob Soccer Modelling/Copa Del Rey")
+source("fotmob_functions.R")
+
+
+########## Variables ########
+
+l <- fotmob_get_league_ids(cached = FALSE)
+View(l)
+
+leagues <- c(365,366,370)
+league_name <- "Copa del Rey"
+ccode <- "ESP"
+team_df <- data.frame(team_1 = c("Sevilla","Girona","Athletic Club","CD Mirandes","Sporting Gijon","Deportivo Alaves","Burgos CF","Malaga","Cartagena","Ponferradina" ),
+                      team_2 = c("Sevilla FC","Girona FC","Athletic Bilbao","Mirandes","Sporting GijÃ³n","AlavÃ©s","Burgos","MÃ¡laga","FC Cartagena","SD Ponferradina" ))
+
+write.csv(team_df,paste0("team_codes",ccode,"_",league_name,"_",".csv"))
+
+team_df2 <-  data.frame(team_1 = c("Sporting de Gijón" ,"Atlético Madrid","Alavés","Valladolid"  ),
+                        team_2 = c("Sporting GijÃ³n" ,"Atletico Madrid","AlavÃ©s","Real Valladolid"  ))
+
+write.csv(team_df2,paste0("team_barstool",ccode,"_",league_name,"_",".csv"))
+
+fixtures <- fotmob_get_league_matches(ccode,league_name, cached = FALSE)
+fixtures <- fixtures[fixtures$status$utcTime>=Sys.Date(),]
+date <- unique(as.Date(fixtures$status$utcTime))
+date <- date[order(date)]
+date <- date[date<(date[1]+3)]
+fixtures <- fixtures[as.Date(fixtures$status$utcTime)<(date[1]+3),]
+f_teams <- read_xlsx("team_df2 3.xlsx")
+
+
+
+
+######### Historical Data ##########
+
+e1_stat <- get_player_data_all(leagues, team_df)
+e1_stats <- e1_stat[[1]]
+
+
+e1_stats_s <- apply(e1_stats,2,as.character)
+write.csv(e1_stats_s,paste0("data_",ccode,"_",league_name,"_",gsub("-","",date[1]),".csv"))
+
+
+e_teams_e <- e1_stat[[2]]
+
+write.csv(e_teams_e,paste0("teams_",ccode,"_",league_name,"_",".csv"))
